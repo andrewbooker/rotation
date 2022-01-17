d14399718c21138cc7e428046bc5ef67ce19655b
Andrew Booker <improvizone@gmail.com>
2022-01-17 18:38:26 +0000

    pulling note-based speed params generation into a class

--- motor.py
+++ motor.py
@@ -6,65 +6,78 @@ import time
 import sys
 from random import randint, random
 
-dcPerNote = {
-	44: 12,
-	45: 13,
-	46: 14,
-	47: 16,
-	48: 18,
-	49: 21,
-	50: 28,
-	51: 46,
-	52: 100
-}
+
+
+class Speed():
+    def next(self):
+        pass
+
+class PitchBased(Speed):
+    dcPerNote = {
+        44: 12,
+        45: 13,
+        46: 14,
+        47: 16,
+        48: 18,
+        49: 21,
+        50: 28,
+        51: 46,
+        52: 100
+    }
+
+    @staticmethod
+    def freq(note):
+        return math.pow(2, (note - 69) / 12.0) * 440
+    
+    def __init__(self, tonic):
+        mode = [2, 2, 1, 2, 2, 1, 2]
+        t = tonic - 12
+        self.notes = [t] if not t < 44 else []
+        for m in range(len(mode)):
+            n = t + mode[m]
+            if not n < 44 and not n > 52:
+                self.notes.append(n)
+            t = n
+        print("using", self.notes)
+
+    def next(self):
+        note = self.notes[randint(0, len(self.notes) - 1)]
+        f = PitchBased.freq(note)
+        dc = PitchBased.dcPerNote[note]
+        print(note, f, dc)
+        return (f, dc)
+
 
 class DiscretePitchMotor():
-	@staticmethod
-	def freq(note):
-		return math.pow(2, (note - 69) / 12.0) * 440
+    def __init__(self, speedSource: Speed):
+        print("starting motor")
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(12, GPIO.OUT)
+        self.pwm = None
+        self.speedSource = speedSource
 
-	def __init__(self, tonic):
-		print("starting motor")
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(12, GPIO.OUT)
-		self.pwm = None
-		self.tonic = tonic
+    def __del__(self):
+        self.stop()
+        GPIO.cleanup()
+        print("motor stopped")
 
-	def __del__(self):
-		self.stop()
-		GPIO.cleanup()
-		print("motor stopped")
+    def start(self, f, dc):
+        if self.pwm is None:
+            self.pwm = GPIO.PWM(12, f)
+            self.pwm.start(dc)
+        else:
+            self.pwm.ChangeFrequency(f)
+            self.pwm.ChangeDutyCycle(dc)
 
-	def start(self, f, dc):
-		if self.pwm is None:
-			self.pwm = GPIO.PWM(12, f)
-			self.pwm.start(dc)
-		else:
-			self.pwm.ChangeFrequency(f)
-			self.pwm.ChangeDutyCycle(dc)
+    def stop(self):
+        if self.pwm is not None:
+            self.pwm.stop()
 
-	def stop(self):
-		if self.pwm is not None:
-			self.pwm.stop()
+    def run(self):
+        while True:
+            (f, dc) = self.speedSource.next()
+            t = 2 + (8 * random())
+            self.start(f, dc)
+            time.sleep(t)
 
-	def run(self):
-		mode = [1, 2, 2, 2, 1, 2, 1]
-		t = self.tonic
-		notes = [t] if not t < 44 else []
-		for m in range(len(mode)):
-			n = t + mode[m]
-			if not n < 44 and not n > 52:
-				notes.append(n)
-			t = n
-
-		print("using", notes)
-		while True:
-			note = notes[randint(0, len(notes) - 1)]
-			f = DiscretePitchMotor.freq(note)
-			dc = dcPerNote[note]
-			t = 2 + (8 * random())
-			self.start(f, dc)
-			print(note, f, dc, t)
-			time.sleep(t)
-
-DiscretePitchMotor(int(sys.argv[1])).run()
+DiscretePitchMotor(PitchBased(int(sys.argv[1]))).run()
