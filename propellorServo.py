efb16e76691896aa9a4dd0a70ebf37d3b9e27231
Andrew Booker <improvizone@gmail.com>
2022-04-16 18:33:40 +0100

    setting random position between min and max servo for serv between min and max

--- propellorServo.py
+++ propellorServo.py
@@ -2,11 +2,15 @@
 
 import RPi.GPIO as GPIO
 import time
+import random
 FREQ_HZ = 50
 MIN = 1.8
 MAX = 9.8
 
 GPIO.setmode(GPIO.BCM)
+
+def anythingBetween(mi, ma):
+    return mi + ((ma - mi) * random.random())
 
 class Device():
     def __init__(self, description, channel):
@@ -22,6 +26,7 @@ class Device():
         print(self.desc, "stopped")
 
     def set(self, dc):
+        print(self.desc, "%0.2f" % dc)
         self.pwm.ChangeDutyCycle(dc)
 
     def stop(self):
@@ -31,12 +36,9 @@ class Device():
 
 
 s = Device("servo", 2)
-time.sleep(5)
-s.set(MIN)
-print("at min")
-time.sleep(5)
-s.set(MAX)
-print("at max")
-time.sleep(5)
+
+while True:
+    time.sleep(5)
+    s.set(anythingBetween(MIN, MAX))
 
 del s
